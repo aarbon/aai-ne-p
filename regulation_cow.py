3fc7a767705b4b6c81f55a76bebcf9b9cd8e8085
Aaron Bonnager <aarbonic@gmail.com>
2012-05-16 13:51:49 -0400

    \<nocomment\/\>

---
+++ regulation_cow.py
@@ -0,0 +1,62 @@
+# TODO flesh out object model:
+# cattle, cows, kine, oxen, Bos taurus
+
+
+#                => ruminant
+#                    => even-toed ungulate, artiodactyl, artiodactyl mammal
+#                        => ungulate, hoofed mammal
+#                            => placental, placental mammal, eutherian, eutherian mammal
+
+#                                    => vertebrate, craniate
+
+
+
+
+
+
+#                                                                => physical entity
+
+
+
+
+
+class Cow(object):
+    
+    def move(self,direction):
+        # dull and slow-moving and stolid;
+        MOVEMENT_SPEED = 1;
+        
+        
+
+    def make_sound(self):
+        
+        # cow, moo-cow
+        print "Moo!"
+
+    def eat(self, food):
+        # 1. ruminant -- (any of various cud-chewing hoofed mammals having a stomach divided into four (occasionally three) compartments)
+        while food is not None:
+            self.chew(food)
+        
+    def chew(self,food):
+        # TODO: write physics model
+        #     do some chewing
+        #     swallow
+        
+        
+        @ How many ruminations does it take to get to the center of 
+        
+
+        return None
+        
+
+
+#
+# # ./wn cow -synsn
+# Sense 1
+# cow, moo-cow
+#       => cattle, cows, kine, oxen, Bos taurus
+# # ./wn cattle -synsn
+
+#       => bovine
+# 2. bovine -- (dull and slow-moving and stolid; like an ox; "showed a bovine apathy")
